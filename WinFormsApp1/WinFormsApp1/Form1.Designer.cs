cd5e4cb0620c3890c6071ed64427120e6febf1e0
JenniSF <JenniferSF@users.noreply.github.com>
2022-05-06 20:29:29 +0200

    Segundo paso

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -29,21 +29,125 @@ namespace WinFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            this.tNombre = new System.Windows.Forms.TextBox();
+            this.tApellidos = new System.Windows.Forms.TextBox();
+            this.tTelefono = new System.Windows.Forms.TextBox();
+            this.tListaSocios = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lNombre = new System.Windows.Forms.Label();
+            this.lApellidos = new System.Windows.Forms.Label();
+            this.lTelefono = new System.Windows.Forms.Label();
+            this.lListaSocios = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // tNombre
+            // 
+            this.tNombre.Location = new System.Drawing.Point(83, 25);
+            this.tNombre.Name = "tNombre";
+            this.tNombre.Size = new System.Drawing.Size(186, 23);
+            this.tNombre.TabIndex = 0;
+            // 
+            // tApellidos
+            // 
+            this.tApellidos.Location = new System.Drawing.Point(407, 25);
+            this.tApellidos.Name = "tApellidos";
+            this.tApellidos.Size = new System.Drawing.Size(186, 23);
+            this.tApellidos.TabIndex = 1;
+            // 
+            // tTelefono
+            // 
+            this.tTelefono.Location = new System.Drawing.Point(695, 25);
+            this.tTelefono.Name = "tTelefono";
+            this.tTelefono.Size = new System.Drawing.Size(186, 23);
+            this.tTelefono.TabIndex = 2;
+            // 
+            // tListaSocios
+            // 
+            this.tListaSocios.Location = new System.Drawing.Point(26, 104);
+            this.tListaSocios.Multiline = true;
+            this.tListaSocios.Name = "tListaSocios";
+            this.tListaSocios.ReadOnly = true;
+            this.tListaSocios.Size = new System.Drawing.Size(855, 279);
+            this.tListaSocios.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(806, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lNombre
+            // 
+            this.lNombre.AutoSize = true;
+            this.lNombre.Location = new System.Drawing.Point(26, 28);
+            this.lNombre.Name = "lNombre";
+            this.lNombre.Size = new System.Drawing.Size(51, 15);
+            this.lNombre.TabIndex = 5;
+            this.lNombre.Text = "Nombre";
+            // 
+            // lApellidos
+            // 
+            this.lApellidos.AutoSize = true;
+            this.lApellidos.Location = new System.Drawing.Point(345, 28);
+            this.lApellidos.Name = "lApellidos";
+            this.lApellidos.Size = new System.Drawing.Size(56, 15);
+            this.lApellidos.TabIndex = 6;
+            this.lApellidos.Text = "Apellidos";
+            // 
+            // lTelefono
+            // 
+            this.lTelefono.AutoSize = true;
+            this.lTelefono.Location = new System.Drawing.Point(636, 28);
+            this.lTelefono.Name = "lTelefono";
+            this.lTelefono.Size = new System.Drawing.Size(53, 15);
+            this.lTelefono.TabIndex = 7;
+            this.lTelefono.Text = "Telefono";
+            // 
+            // lListaSocios
+            // 
+            this.lListaSocios.AutoSize = true;
+            this.lListaSocios.Location = new System.Drawing.Point(26, 79);
+            this.lListaSocios.Name = "lListaSocios";
+            this.lListaSocios.Size = new System.Drawing.Size(68, 15);
+            this.lListaSocios.TabIndex = 8;
+            this.lListaSocios.Text = "Lista Socios";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(917, 443);
+            this.Controls.Add(this.lListaSocios);
+            this.Controls.Add(this.lTelefono);
+            this.Controls.Add(this.lApellidos);
+            this.Controls.Add(this.lNombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tListaSocios);
+            this.Controls.Add(this.tTelefono);
+            this.Controls.Add(this.tApellidos);
+            this.Controls.Add(this.tNombre);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tNombre;
+        private System.Windows.Forms.TextBox tApellidos;
+        private System.Windows.Forms.TextBox tTelefono;
+        private System.Windows.Forms.TextBox tListaSocios;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lNombre;
+        private System.Windows.Forms.Label lApellidos;
+        private System.Windows.Forms.Label lTelefono;
+        private System.Windows.Forms.Label lListaSocios;
     }
 }
 
