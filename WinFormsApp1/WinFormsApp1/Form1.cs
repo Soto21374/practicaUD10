f24c567eedd023e71766ea1782554acd534ea7ab
JenniSF <92455801+JenniSF@users.noreply.github.com>
2022-05-06 20:50:21 +0200

    Tercer paso

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -21,5 +21,10 @@ namespace WinFormsApp1
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tListaSocios.Text = (tNombre.Text + tApellidos.Text + tTelefono.Text);
+        }
     }
 }
